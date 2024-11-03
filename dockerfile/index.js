8a9110f75496c25d4953082a0988c3044ced7886
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2024-11-03 15:29:06 -0300

    Using cache to test the response

--- dockerfile/index.js
+++ dockerfile/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const APP = express();
 const PORT = 3000;
 
-APP.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+APP.get('/', (req, res) => {
+    const CACHE = path.join(__dirname, "cache");
+    const INDEX = path.join(CACHE, "index.html");
+
+    if(!fs.existsSync(INDEX)){
+        // Simulating a process with a delay
+        fs.mkdirSync(CACHE, { recursive: true });
+        setTimeout(() => {
+            fs.writeFileSync(INDEX, '<h1>Hello, World!</h1>', { encoding: "utf-8" });
+            res.sendFile(INDEX);
+        }, 3000);
+    }
+    else res.sendFile(INDEX);
+});
 
 APP.listen(PORT, () => console.log(`Listening on ${PORT}...`));
